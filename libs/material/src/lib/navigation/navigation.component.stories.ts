d49e88a5c2a90c15349027f86164d4c0f4b075d3
nx-ng-nest <101035559+nx-ng-nest@users.noreply.github.com>
2022-03-17 20:04:49 -0500

    storybook

--- libs/material/src/lib/navigation/navigation.component.stories.ts
+++ libs/material/src/lib/navigation/navigation.component.stories.ts
@@ -1,26 +1,51 @@
-import { moduleMetadata, Story, Meta } from '@storybook/angular';
+import { LayoutModule } from '@angular/cdk/layout';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {
+  Meta,
+  moduleMetadata,
+  Story,
+} from '@storybook/angular';
+
 import { NavigationComponent } from './navigation.component';
 
 export default {
   title: 'NavigationComponent',
   component: NavigationComponent,
+
   decorators: [
     moduleMetadata({
-      imports: [],
-    })
+      imports: [
+        CommonModule,
+        BrowserAnimationsModule,
+        LayoutModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatSidenavModule,
+        MatIconModule,
+        MatListModule,
+      ],
+    }),
   ],
 } as Meta<NavigationComponent>;
 
 const Template: Story<NavigationComponent> = (args: NavigationComponent) => ({
   props: args,
+
 });
 
-
 export const Primary = Template.bind({});
+
 Primary.args = {
-    navigationMenu:  [
+  navigationMenu: [
     { path: 'products', icon: 'inventory' },
     { path: 'users', icon: 'people' },
     { path: 'config', icon: 'settings' },
   ],
-}
+};
